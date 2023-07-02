be995cc7571bdaf2ca42254b7b5286fbde6a1357
Kerry Wang <kerrywang369@gmail.com>
2023-07-02 01:52:41 -0600

    getting output

--- main.py
+++ main.py
@@ -56,15 +56,75 @@ def stringify_range(node: ast.Call, iteratorName: str):
         + " < "
         + node.args[1].id
         + "; i++) {"
+        + "\n"
     )
 
 
-def parse_for(node: ast.For, indent: int = 0):
+def parse_for(node: ast.For, indent: int = 0) -> str:
     buffer = (
         "For(int " + node.target.id + " " + stringify_range(node.iter, node.target.id)
     )
-    for child in node.body:
-        buffer += indentify(indent, dump(child)) + "\n"
+    buffer += parse_body(node.body, indent + 1)
+    return buffer
+
+
+def parse_targets(nodes: list[ast.AST], indent: int = 0) -> str:
+    buffer = ""
+    for node in nodes:
+        if isinstance(node, ast.Subscript):
+            buffer += parse_subscript(node, indent)
+        else:
+            print("Error: unexpected target")
+    return buffer
+
+
+def parse_assign(node: ast.Assign, indent: int = 0) -> str:
+    buffer = indentify(indent, "")
+    buffer += parse_targets(node.targets, indent)
+    buffer += " = "
+    buffer += parse_value(node.value, indent)
+    buffer += "\n"
+    return buffer
+
+
+def parse_body(nodes: list[ast.AST], indent: int = 0) -> str:
+    buffer = ""
+    for node in nodes:
+        # buffer += indentify(indent, dump(child)) + "\n"
+        if isinstance(node, ast.Assign):
+            buffer += parse_assign(node, indent)
+    return buffer
+
+
+def parse_slice(node: ast.AST, indent: int = 0) -> str:
+    if isinstance(node, ast.Name):
+        return node.id
+    if isinstance(node, ast.BinOp):
+        return parse_slice(node.left) + " + " + parse_slice(node.right)
+    if isinstance(node, ast.Constant):
+        return str(node.value)
+    print("Error: unexpected slice", type(node))
+    return ""
+
+
+def parse_value(node: ast.AST, indent: int = 0) -> str:
+    if isinstance(node, ast.Constant):
+        # print("a")
+        return str(node.value)
+    if isinstance(node, ast.Name):
+        # print("b", node.id)
+        return node.id
+    if isinstance(node, ast.Subscript):
+        # print("c")
+        return parse_subscript(node, indent)
+    return ""
+
+
+def parse_subscript(node: ast.Subscript, indent: int = 0) -> str:
+    if not node:
+        return ""
+    buffer = parse_value(node.value, indent)
+    buffer += parse_slice(node.slice, indent)
     return buffer
 
 
