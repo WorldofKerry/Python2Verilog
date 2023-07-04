bd1d81c75563334f6414a85c12444e854a76f67a
Kerry Wang <kerrywang369@gmail.com>
2023-07-04 14:41:27 -0600

    output data

---
+++ tests/rectangle_filled.py
@@ -0,0 +1,60 @@
+from setup import python2verilog as p2v, output, input
+
+code = ""
+
+code = """
+def draw_rectangle(s_x, s_y, height, width) -> tuple[int, int]:
+    for i0 in range(0, width):
+        for i1 in range(0, height): 
+            yield (s_x + i1, s_y + i0)
+
+
+"""
+exec(code)
+rows, cols = (20, 20)
+generator = draw_rectangle(1, 2, 3, 4)
+grid = [[0 for i in range(cols)] for j in range(rows)]
+
+with output(__file__, "expected", "csv") as f:
+    for x, y in generator:
+        grid[x][y] = 1
+        f.write(f"{x}, {y}\n")
+
+with output(__file__, "visual") as f:
+    if grid:
+        for row in grid:
+            for e in row:
+                f.write(f"{e}")
+            f.write("\n")
+
+    import ast
+
+    tree = ast.parse(code)
+    f.write(ast.dump(tree, indent=4))
+
+with output(__file__, "module", "sv") as f:
+    from setup import python2verilog as p2v
+
+    func = tree.body[0]
+    genParser = p2v.GeneratorParser(func)
+    f.write(str(genParser.generate_verilog()))
+
+import csv
+
+with input(__file__, "expected", "csv") as exp_f:
+    with input(__file__, "actual", "csv") as act_f:
+        expected = csv.reader(exp_f)
+        actual = csv.reader(act_f)
+
+        exp_coords = set()
+        act_coords = set()
+
+        for row in actual:
+            if row[1].strip() != "x" and row[2].strip() != "x":
+                exp_coords.add((row[1].strip(), row[2].strip()))
+
+        for row in expected:
+            act_coords.add((row[0].strip(), row[1].strip()))
+
+        assert not (exp_coords - act_coords)
+        assert not (act_coords - exp_coords)
---
+++ tests/rectangle_filled_module.sv
@@ -0,0 +1,60 @@
+module draw_rectangle(
+    input wire _clock,
+    input wire _start,
+    input wire [31:0] s_x,
+    input wire [31:0] s_y,
+    input wire [31:0] height,
+    input wire [31:0] width,
+    output reg [31:0] _out0,
+    output reg [31:0] _out1,
+    output reg _done
+);
+    reg [31:0] _STATE;
+    reg [31:0] _STATE_0;
+    reg [31:0] i0;
+    reg [31:0] _INNER_1_STATE;
+    reg [31:0] _INNER_1_STATE_0;
+    reg [31:0] i1;
+    reg [31:0] _INNER_1_INNER_2_STATE;
+    reg [31:0] _INNER_1_INNER_2_STATE_0;
+        always @(posedge _clock) begin
+            if (_start) begin
+                _done <= 0;
+                _STATE <= 0;
+                _STATE_0 <= 0;
+                i0 <= 0;
+                _INNER_1_STATE <= 0;
+                _INNER_1_STATE_0 <= 0;
+                i1 <= 0;
+                _INNER_1_INNER_2_STATE <= 0;
+                _INNER_1_INNER_2_STATE_0 <= 0;
+            end else begin
+                case (_STATE) // STATEMENTS START
+                    _STATE_0: begin
+                        if (i0 >= width) _STATE = _STATE + 1; // FOR LOOP START
+                        else begin
+                            case (_INNER_1_STATE) // STATEMENTS START
+                                _INNER_1_STATE_0: begin
+                                    if (i1 >= height) _INNER_1_STATE = _INNER_1_STATE + 1; // FOR LOOP START
+                                    else begin
+                                        case (_INNER_1_INNER_2_STATE) // STATEMENTS START
+                                            _INNER_1_INNER_2_STATE_0: begin
+                                                _out0 <= s_x + i1;
+                                                _out1 <= s_y + i0;
+                                                _INNER_1_INNER_2_STATE <= _INNER_1_INNER_2_STATE + 1;
+                                                _INNER_1_INNER_2_STATE <= 0;
+                                                i1 <= i1 + 1;
+                                            end
+                                        endcase // STATEMENTS END
+                                    end // FOR LOOP END
+                                    _INNER_1_STATE <= 0;
+                                    i0 <= i0 + 1;
+                                end
+                            endcase // STATEMENTS END
+                        end // FOR LOOP END
+                        _done = 1;
+                    end
+                endcase // STATEMENTS END
+            end
+        end
+endmodule
