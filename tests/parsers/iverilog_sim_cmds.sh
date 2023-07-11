f060bf493276c544e92438bfff964b5842a04639
Kerry Wang <kerrywang369@gmail.com>
2023-07-11 16:45:45 -0600

    hopefully fixed CI issue

--- tests/parsers/iverilog_sim_cmds.sh
+++ tests/parsers/iverilog_sim_cmds.sh
@@ -1,4 +1,9 @@
-iverilog -s generator_tb /home/kerrwang/repos/testing1/tests/parsers/data/generator/delete_me/module.sv /home/kerrwang/repos/testing1/tests/parsers/data/generator/delete_me/testbench.sv && unbuffer vvp a.out > /home/kerrwang/repos/testing1/tests/parsers/data/generator/delete_me/actual.csv && cat /home/kerrwang/repos/testing1/tests/parsers/data/generator/delete_me/actual.csv
-iverilog -s generator_tb /home/kerrwang/repos/testing1/tests/parsers/data/generator/circle_lines/module.sv /home/kerrwang/repos/testing1/tests/parsers/data/generator/circle_lines/testbench.sv && unbuffer vvp a.out > /home/kerrwang/repos/testing1/tests/parsers/data/generator/circle_lines/actual.csv && cat /home/kerrwang/repos/testing1/tests/parsers/data/generator/circle_lines/actual.csv
-iverilog -s generator_tb /home/kerrwang/repos/testing1/tests/parsers/data/generator/rectangle_lines/module.sv /home/kerrwang/repos/testing1/tests/parsers/data/generator/rectangle_lines/testbench.sv && unbuffer vvp a.out > /home/kerrwang/repos/testing1/tests/parsers/data/generator/rectangle_lines/actual.csv && cat /home/kerrwang/repos/testing1/tests/parsers/data/generator/rectangle_lines/actual.csv
-iverilog -s generator_tb /home/kerrwang/repos/testing1/tests/parsers/data/generator/rectangle_filled/module.sv /home/kerrwang/repos/testing1/tests/parsers/data/generator/rectangle_filled/testbench.sv && unbuffer vvp a.out > /home/kerrwang/repos/testing1/tests/parsers/data/generator/rectangle_filled/actual.csv && cat /home/kerrwang/repos/testing1/tests/parsers/data/generator/rectangle_filled/actual.csv
+#!/bin/bash
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$parent_path"
+
+
+iverilog -s generator_tb data/generator/delete_me/module.sv data/generator/delete_me/testbench.sv && unbuffer vvp a.out > data/generator/delete_me/actual.csv && cat data/generator/delete_me/actual.csv
+iverilog -s generator_tb data/generator/rectangle_lines/module.sv data/generator/rectangle_lines/testbench.sv && unbuffer vvp a.out > data/generator/rectangle_lines/actual.csv && cat data/generator/rectangle_lines/actual.csv
+iverilog -s generator_tb data/generator/rectangle_filled/module.sv data/generator/rectangle_filled/testbench.sv && unbuffer vvp a.out > data/generator/rectangle_filled/actual.csv && cat data/generator/rectangle_filled/actual.csv
+iverilog -s generator_tb data/generator/circle_lines/module.sv data/generator/circle_lines/testbench.sv && unbuffer vvp a.out > data/generator/circle_lines/actual.csv && cat data/generator/circle_lines/actual.csv
