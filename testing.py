9ce0f8968eaea5ed8d18bbc1fa1111acc7af9fb8
Kerry Wang <43691721+WorldofKerry@users.noreply.github.com>
2023-07-13 13:59:27 -0600

    updated readme (#29)

--- testing.py
+++ testing.py
@@ -12,6 +12,62 @@
 # child = TestChild()
 
 
+# class A:
+#     def __init__(self, x):
+#         print("__init__ is called in A")
+#         self.x = x
+
+
+# class B:
+#     def __init__(self, *args, **kwargs):
+#         print("__init__ is called in B")
+#         super().__init__(*args, **kwargs)
+
+
+# class AB(B, A):
+#     def __init__(self, *args, **kwargs):
+#         print("__init__ is called in AB")
+#         super().__init__(*args, **kwargs)
+
+
+# ab = AB(123)
+
+from python2verilog.frontend.generator import GeneratorParser
+import ast
+
+func = """
+def circle_lines(s_x, s_y, height) -> tuple[int, int]:
+    x = 0
+    y = height
+    d = 3 - 2 * height
+    yield (s_x + x, s_y + y)
+    yield (s_x + x, s_y - y)
+    yield (s_x - x, s_y + y)
+    yield (s_x - x, s_y - y)
+    yield (s_x + y, s_y + x)
+    yield (s_x + y, s_y - x)
+    yield (s_x - y, s_y + x)
+    yield (s_x - y, s_y - x)
+    while y >= x:
+        x = x + 1
+        if d > 0:
+            y = y - 1
+            d = d + 4 * (x - y) + 10
+        else:
+            d = d + 4 * x + 6
+        yield (s_x + x, s_y + y)
+        yield (s_x + x, s_y - y)
+        yield (s_x - x, s_y + y)
+        yield (s_x - x, s_y - y)
+        yield (s_x + y, s_y + x)
+        yield (s_x + y, s_y - x)
+        yield (s_x - y, s_y + x)
+        yield (s_x - y, s_y - x)
+"""
+generatorParser = GeneratorParser(ast.parse(func).body[0])
+print(generatorParser.generate_verilog())
+
+
 class A:
     def __init__(self, x):
         print("__init__ is called in A")
