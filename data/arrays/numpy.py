e7306e7455f053e29eff145e9c5470022f3e5cee
Kerry Wang <kerrywang369@gmail.com>
2023-08-16 11:30:13 -0600

    ast testing

---
+++ data/arrays/numpy.py
@@ -0,0 +1,13 @@
+code = """
+import numpy as np
+val = np.ndarray(10)
+
+def func(input: np.ndarray) -> tuple[np.ndarray]:
+  return np.ndarray(15)
+"""
+
+import ast
+
+tree = ast.parse(code)
+print(tree)
+print(ast.dump(tree, indent=1))
