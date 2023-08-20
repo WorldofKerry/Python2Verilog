95fbeea2db11225639da07dc8e657816bb878f0e
Kerry Wang <kerrywang369@gmail.com>
2023-08-19 18:17:09 -0600

    updated prototype

--- research/decorators.py
+++ research/decorators.py
@@ -1,18 +1,32 @@
 import inspect
+import logging
 
-functions = {}
+gen_funcs = {}
 
 
-def verilogify(func):
+def my_dec(func):
     def generator_wrapper(*args, **kwargs):
-        functions[func] = {"func": func, "source": inspect.getsource(func)}
-        for result in func(*args, **kwargs):
-            yield result
+        print(f"Called with {str(*args)} {str(**kwargs)}")
 
-    return generator_wrapper
+        gen_funcs[func] = {"func": func, "source": inspect.getsource(func)}
+
+        def __generator(*args, **kwargs):
+            """
+            Required to executate function up until first yield
+            """
+            for result in func(*args, **kwargs):
+                yield result
+
+        return __generator(*args, **kwargs)
+
+    def function_wrapper(*args, **kwargs):
+        logging.error("Non-generator functions currently not supported")
+        return func(*args, **kwargs)
+
+    return generator_wrapper if inspect.isgeneratorfunction(func) else function_wrapper
 
 
-@verilogify
+@my_dec
 def my_counter(n):
     i = 0
     while i < n:
@@ -22,5 +36,9 @@ def my_counter(n):
 
 for value in my_counter(5):
     print(value)
+inst = my_counter(10)
+next(inst)
 
-print(functions)
+print(gen_funcs)
+for key, value in gen_funcs.items():
+    print(type(key), type(value))
