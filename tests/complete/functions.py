0220ea578ec31722ec4b57caa6a2fe4fd81d8dfe
Kerry Wang <kerrywang369@gmail.com>
2023-09-19 11:33:03 -0600

    WIP parameterized tests

---
+++ tests/complete/functions.py
@@ -0,0 +1,40 @@
+def circle_lines(s_x, s_y, height):
+    x = 0
+    y = height
+    d = 3 - 2 * y
+    yield (s_x + x, s_y + y)
+    yield (s_x + x, s_y - y)
+    yield (s_x - x, s_y + y)
+    yield (s_x - x, s_y - y)
+    yield (s_x + y, s_y + x)
+    yield (s_x + y, s_y - x)
+    yield (s_x - y, s_y + x)
+    yield (s_x - y, s_y - x)
+    while y >= x:
+        x = x + 1
+        if d > 0:
+            y = y - 1
+            d = d + 4 * (x - y) + 10
+        else:
+            d = d + 4 * x + 6
+        yield (s_x + x, s_y + y)
+        yield (s_x + x, s_y - y)
+        yield (s_x - x, s_y + y)
+        yield (s_x - x, s_y - y)
+        yield (s_x + y, s_y + x)
+        yield (s_x + y, s_y - x)
+        yield (s_x - y, s_y + x)
+        yield (s_x - y, s_y - x)
+
+
+def fib(n: int):
+    a = 0
+    b = 1
+    c = 0
+    count = 1
+    while count < n:
+        count += 1
+        a = b
+        b = c
+        c = a + b
+        yield c
