4eab09425a56356885584c32c565309d37eda226
Kerry Wang <kerrywang369@gmail.com>
2023-10-23 14:34:21 -0600

    full example

--- tests/optimizer/test_graph.py
+++ tests/optimizer/test_graph.py
@@ -4,8 +4,8 @@ import unittest
 import networkx as nx
 from matplotlib import pyplot as plt
 
-from python2verilog.ir.expressions import *
-from python2verilog.ir.graph2 import *
+from python2verilog.ir.expressions import *  # nopycln: import
+from python2verilog.ir.graph2 import *  # nopycln: import
 
 
 class TestGraph(unittest.TestCase):
@@ -14,6 +14,7 @@ class TestGraph(unittest.TestCase):
         i = Var("i")
         n = Var("n")
         a = Var("a")
+        b = Var("b")
 
         graph = Graph()
 
@@ -29,6 +30,12 @@ class TestGraph(unittest.TestCase):
         prev = graph.add_node(AssignNode(out, a), prev)
         prev = graph.add_node(ClockNode(), prev)
         graph.add_node(FalseNode(), if_a_mod_2, children=[prev])
+        a_assign_b = graph.add_node(AssignNode(a, b), prev)
+        b_assign_a_plus_b = graph.add_node(AssignNode(b, BinOp(a, "+", b)), prev)
+        prev = graph.add_node(ClockNode(), a_assign_b, b_assign_a_plus_b)
+        prev = graph.add_node(
+            AssignNode(i, BinOp(i, "+", Int(1))), prev, children=[if_i_lt_n]
+        )
 
         logging.error(prev)
         logging.error(graph)
