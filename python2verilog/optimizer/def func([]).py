9893b27c203c1688634490a4c9b9e7db764b501a
Kerry Wang <kerrywang369@gmail.com>
2023-10-30 18:05:30 -0600

    next state may need additional global vars

--- python2verilog/optimizer/def func([]).py
+++ python2verilog/optimizer/def func([]).py
@@ -1,26 +1,16 @@
-def func(a0, b0):
-  i0 = 0
-  c0 = 0
-  i1 = 0
-  c1 = 0
-  if (i1 < a0):
-    c2 = (0 + b0)
-    i2 = 1
-    i1 = 1
-    c1 = (0 + b0)
-    if (i1 < a0):
-      c2 = ((0 + b0) + b0)
-      i2 = 2
-      i1 = 2
-      c1 = ((0 + b0) + b0)
-      if (i1 < a0):
-        c2 = (((0 + b0) + b0) + b0)
-        i2 = 3
-        i1 = 3
-        c1 = (((0 + b0) + b0) + b0)
-      else:
-        return [(0 + b0) + b0]
+def func(i1, c1):
+    if i1 < a0:
+        c2 = c1 + b0
+        i2 = i1 + 1
+        if (i1 + 1) < a0:
+            c2 = (c1 + b0) + b0
+            i2 = (i1 + 1) + 1
+            if ((i1 + 1) + 1) < a0:
+                c2 = ((c1 + b0) + b0) + b0
+                i2 = ((i1 + 1) + 1) + 1
+            else:
+                return [(c1 + b0) + b0]
+        else:
+            return [c1 + b0]
     else:
-      return [0 + b0]
-  else:
-    return [0]
+        return [c1]
