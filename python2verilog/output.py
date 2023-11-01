05e7dbbef4908b31611090f3598e1c68c2452f49
Kerry Wang <kerrywang369@gmail.com>
2023-11-01 16:11:28 -0600

    minor

--- python2verilog/output.py
+++ python2verilog/output.py
@@ -1,15 +1,29 @@
-def func128(a1, b1, i1, n0):
+def func139(a1, b1, i1, n0):
     if i1 < n0:
         if a1 % 2:
             yield [a1]
-            yield from func153(n0, a1, b1, i1)
+            yield from func175(b1, n0, a1, i1)
         else:
             if (i1 + 1) < n0:
                 if b1 % 2:
                     yield [b1]
-                    yield from func153(n0, b1, a1 + b1, i1 + 1)
+                    yield from func175(a1 + b1, n0, b1, i1 + 1)
                 else:
-                    yield from func128(a1 + b1, b1 + (a1 + b1), (i1 + 1) + 1, n0)
+                    if ((i1 + 1) + 1) < n0:
+                        if (a1 + b1) % 2:
+                            yield [a1 + b1]
+                            yield from func175(
+                                b1 + (a1 + b1), n0, a1 + b1, (i1 + 1) + 1
+                            )
+                        else:
+                            yield from func139(
+                                b1 + (a1 + b1),
+                                (a1 + b1) + (b1 + (a1 + b1)),
+                                ((i1 + 1) + 1) + 1,
+                                n0,
+                            )
+                    else:
+                        return
             else:
                 return
     else:
@@ -20,27 +34,46 @@ def func101(n0):
     if 0 < n0:
         if 0:
             yield [0]
-            yield from func153(n0, 0, 1, 0)
+            yield from func175(1, n0, 0, 0)
         else:
             if 1 < n0:
                 if 1:
                     yield [1]
-                    yield from func153(n0, 1, 1, 1)
+                    yield from func175(1, n0, 1, 1)
                 else:
-                    yield from func128(1, 2, 2, n0)
+                    if 2 < n0:
+                        if 1:
+                            yield [1]
+                            yield from func175(2, n0, 1, 2)
+                        else:
+                            yield from func139(2, 3, 3, n0)
+                    else:
+                        return
             else:
                 return
     else:
         return
 
 
-def func153(n0, a1, b1, i1):
+def func175(b1, n0, a1, i1):
     if (i1 + 1) < n0:
         if b1 % 2:
             yield [b1]
-            yield from func153(n0, b1, a1 + b1, i1 + 1)
+            yield from func175(a1 + b1, n0, b1, i1 + 1)
         else:
-            yield from func128(a1 + b1, b1 + (a1 + b1), (i1 + 1) + 1, n0)
+            if ((i1 + 1) + 1) < n0:
+                if (a1 + b1) % 2:
+                    yield [a1 + b1]
+                    yield from func175(b1 + (a1 + b1), n0, a1 + b1, (i1 + 1) + 1)
+                else:
+                    yield from func139(
+                        b1 + (a1 + b1),
+                        (a1 + b1) + (b1 + (a1 + b1)),
+                        ((i1 + 1) + 1) + 1,
+                        n0,
+                    )
+            else:
+                return
     else:
         return
 
--- tests/optimizer/test_graph2.py
+++ tests/optimizer/test_graph2.py
@@ -424,7 +424,7 @@ class TestGraph(unittest.TestCase):
         lowered = CFG()
         lowered = (
             graph
-            | make_single_end_per_subgraph(threshold=1)
+            | make_single_end_per_subgraph(threshold=2)
             # | lower_to_fsm(threshold=1)
             # | insert_merge_nodes()
             # | insert_phis()
